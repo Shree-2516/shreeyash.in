312961f309ff59751e53a0c6521957c1def3198b
Shreeyash <shreeyash2573@gmail.com>
2026-06-03 14:57:31 +0530

    Updated project and deployment changes

--- admin.py
+++
@@ -1,6 +0,0 @@
-from app import create_app
-
-app = create_app(include_main=False, include_admin=True, admin_prefix="")
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)
--- user.py
+++
@@ -1,6 +0,0 @@
-from app import create_app
-
-app = create_app(include_main=True, include_admin=False)
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
